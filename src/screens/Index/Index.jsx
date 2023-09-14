654a6798b740f1d0c3dedb449c4925f611d08e81
mohammadhamzailtaf <144703210+mohammadhamzailtaf@users.noreply.github.com>
2023-09-14 08:13:29 -0700

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <div className="component">
+            <div className="rectangle" />
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <div className="text-wrapper">Phone Number*</div>
+            <div className="rectangle-4" />
+            <div className="text-wrapper-2">Select State*</div>
+            <div className="overlap-group">
+              <img
+                className="path"
+                alt="Path"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/path-4.svg"
+              />
+              <div className="text-wrapper-3">+91</div>
+            </div>
+            <div className="overlap-2">
+              <div className="rectangle-5" />
+              <div className="save-address">Save Address &amp; Continue</div>
+            </div>
+            <div className="text-wrapper-4">Name*</div>
+            <div className="text-wrapper-5">Landmark</div>
+            <div className="text-wrapper-6">Pincode*</div>
+            <div className="text-wrapper-7">City*</div>
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="text-wrapper-8">Address*</div>
+            <div className="overlap-3">
+              <div className="rectangle-8" />
+              <div className="text-wrapper-9">Free Shipping</div>
+            </div>
+          </div>
+          <div className="component-2">
+            <div className="text-wrapper-10">Enter Your Full Name</div>
+            <div className="text-wrapper-11">Enter Phone Number</div>
+            <p className="p">Enter House No. / Street Address</p>
+            <p className="text-wrapper-12">Enter Landmark Near Your Home</p>
+            <div className="text-wrapper-13">Enter City</div>
+            <div className="text-wrapper-14">Enter Pincode</div>
+            <div className="text-wrapper-15">Select Your State</div>
+          </div>
+          <img
+            className="img"
+            alt="Component"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/component-31---1.svg"
+          />
+          <img
+            className="component-3"
+            alt="Component"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65032170daf4d0c7dc24ef27/img/component-34.svg"
+          />
+          <div className="rectangle-9" />
+        </div>
+        <div className="div-wrapper">
+          <p className="text-wrapper-16">Sale Is Live | Upto 98% Discount On Phones</p>
+        </div>
+        <img
+          className="icon-ionic-ios-arrow"
+          alt="Icon ionic ios arrow"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65032170daf4d0c7dc24ef27/img/icon-ionic-ios-arrow-back.svg"
+        />
+        <div className="text-wrapper-17">Add delivery address</div>
+      </div>
+    </div>
+  );
+};
